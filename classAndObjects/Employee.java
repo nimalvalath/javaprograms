406bda30d0b73f15498f2f8ce387c63697808d31
nimalvalath <nimal.j.valath@gmail.com>
2017-12-01 12:07:46 +0530

    Update Employee.java

--- classAndObjects/Employee.java
+++ classAndObjects/Employee.java
@@ -1,55 +1,64 @@
 public class Employee{
-	int id;
-	String name;
-	double salary;
-	int mobNumber;
-	String address;
-	
+  private  int id;
+    private  String name;
+    private double salary;
+    private String gender;
+    private String[] taskList;
 
 
-	Employee(){
-		id=-1;
-		name="Not defined";
-		salary=-1;
-		mobNumber=0;
-		address="not defined";
-	}
+    Employee(){
+        id=-1;
+        name="Not defined";
+        salary=-1;
 
-	Employee(String name,double salary,String address){
-		//explicit is called here always
-		//this(); 
-				/*this.id=id;
-		this.salary=salary;*/
-		this(123,name,salary,0,address);	
-	}
-	/*we can aslo call
-		Employee(int id){
-		
-		this(id,"name",-2);
-		
-		}
-		//this(id,name,salary)--> 3 params constructor like this
-		//
-	*/
-	Employee(int id, String name, double salary,int mobNumber, String address){
-		this.id=id;
-		this.name=name;
-		this.salary=salary;
-		this.mobNumber=mobNumber;
-		this.address=address;
-	}
+        gender="not defined";
+        taskList=new String[5];
+    }
 
-	public void doSomething(){
-		System.out.println("Employee is doing something");
-	}
-	public int getId(){
-		return id;
-	}
-	public String getName(){
-		return name;
-	}
-	public double getSalary(){
-		return salary;
-	}
+    Employee(String name,double salary,String gender){
+
+        this(123,name,salary,gender);
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getTaskList() {
+        return String.join(",",taskList);
+
+    }
+
+    public void setTaskList(String[] taskList) {
+        this.taskList = taskList;
+    }
+
+    Employee(int id, String name, double salary, String address){
+        this.id=id;
+        this.name=name;
+
+        this.salary=salary;
+        this.gender=gender;
+        taskList=new String[5];
+
+    }
+
+    public void doSomething(){
+        System.out.println("Employee is doing some tasks");
+    }
+  public String printEmployeeDetails(){
+        return "id:-->"+id+" name:-->"+name+" gender:-->"+gender+" salary:-->"+salary;
+  }
+   public static void raiseSalary(Employee emp,int percentage){
+      
+      double newSalary=emp.getSalary();
+      newSalary+=newSalary*(percentage/100);
+      emp.setSalary(newSalary);
+
+   }
 
 }
